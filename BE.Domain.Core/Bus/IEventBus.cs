fcf9b80fe85bdc6d463f8c9b1410d16f6910d8ed
Akhi Kumar <akhilsingh.ims@gmail.com>
2024-04-03 15:59:09 +0530

    Changes in domain layer

---
+++ BE.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,22 @@
+﻿using BE.Domain.Core.Commands;
+using BE.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+         Task SendCommand<T>(T command) where T:Command;
+
+
+        void Publish<T> (T @event)where T:Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
