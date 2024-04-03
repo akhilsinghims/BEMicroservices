360ed3ba66f9b0277d7d3500c88bf1bce09b55dc
Akhi Kumar <akhilsingh.ims@gmail.com>
2024-04-03 19:48:41 +0530

    Initieal commit Infra.IoC

---
+++ BE.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using BE.Domain.Core.Bus;
+using BE.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMqBus>();
+        }
+    }
+}
